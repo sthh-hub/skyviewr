e8208b04b73f54ae2ad28869b2a1a255ecac4b5e
Sherry <tsai.hsinh@northeastern.edu>
2025-01-17 20:19:19 -0800

    import dotenv, server run on port

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,2 +1,16 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+// import weatherRoutes from './routes/weather';
+// import interactionRoutes from './routes/interaction';
 import dotenv from 'dotenv';
-dotenv.config();
+
+const app = express();
+app.use(bodyParser.json());
+
+// app.use('/api/weather', weatherRoutes);
+// app.use('/api/interactions', interactionRoutes);
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
